cac75a1a5f8aca2ca5666a74deb43fa8d951fe00
Nery Eduardo Herrera <tesco.herrera.edu@gmai.com>
2021-09-26 21:44:47 -0600

    [ADD] ingerso de reporte estructura del js

--- Practica2/frontend/src/Components/ingresoReporte.js
+++ Practica2/frontend/src/Components/ingresoReporte.js
@@ -1,18 +1,54 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { Form, Button } from 'react-bootstrap';
+import { useState, useEffect } from 'react';
 
-function ingresoReporte() {
-  return (
-    <Form>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
-        </Form.Group>
+function IngresoReporte() {
 
-        <Button variant="primary" type="submit">
-            Submit
-        </Button>
-    </Form>
-  );
+    const [carnet, setCarnet] = useState('');
+    const [nombre, setNombre] = useState('');
+    const [curso, setCurso] = useState('');
+    const [reporte, setReporte] = useState('');
+
+    const enviar = async() => {
+        /*const res = await axios.post(`url`, {
+            carnet:carnet,
+            nombre:nombre, 
+            curso:curso, 
+            fecha:"26/09/2021", 
+            cuerpo:reporte
+        } ).then(
+            function (response) {
+                console.log(response);
+            }
+        );*/
+        console.log(carnet);
+    }
+
+    useEffect( () => {
+        console.log(carnet)
+    }, [carnet] ) 
+
+    useEffect( () => {
+        console.log(nombre)
+    }, [nombre] )
+
+    return (
+        <Form>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>carnet</Form.Label>
+                <Form.Control type="text" placeholder="carnet" value={carnet} onChange={event => setCarnet(event.target.value)} />
+                <Form.Label>nombre</Form.Label>
+                <Form.Control type="text" placeholder="nombre" value={nombre} onChange={event => setNombre(event.target.value)} />
+                <Form.Label>curso</Form.Label>
+                <Form.Control type="text" placeholder="curso" value={curso} onChange={event => setCurso(event.target.value)} />
+                <Form.Label>Cuerpo de reporte</Form.Label>
+                <Form.Control type="text" placeholder="reporte" value={reporte} onChange={event => setReporte(event.target.value)} />
+            </Form.Group>
+            <Button variant="primary" onClick={ enviar }>
+                Submit
+            </Button>
+        </Form>
+    );
 }
 
-export default ingresoReporte;
+export default IngresoReporte;
--- Practica2/frontend/src/index.js
+++ Practica2/frontend/src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import ingresoReporte from './Components/ingresoReporte';
+import IngresoReporte from './Components/ingresoReporte';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <ingresoReporte />
+    <IngresoReporte />
   </React.StrictMode>,
   document.getElementById('root')
 );
