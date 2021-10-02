d065df64e1ff5acb1911262e9df3a1951d8403cc
Oscar Llamas <oscarllamasusac@gmail.com>
2021-10-02 15:39:38 -0600

    [UPDATED] Se modificó Dockerfile y Docker Compose para el frontend.

--- Practica2/frontend/src/Components/IngresoReporte.js
+++ Practica2/frontend/src/Components/IngresoReporte.js
@@ -10,8 +10,10 @@ function IngresoReporte() {
     const [curso, setCurso] = useState('');
     const [reporte, setReporte] = useState('');
 
+    let balancer = process.env.HOST || 'localhost';
+
     const enviar = async() => { 
-        const res = await axios.post(`http://34.125.190.196:8080/api/create/reporte`, {
+        const res = await axios.post(`http://${balancer}:8080/api/create/reporte`, {
         //const res = await axios.post(`/api/create/reporte`, {    
             carnet:carnet,
             nombre:nombre, 
