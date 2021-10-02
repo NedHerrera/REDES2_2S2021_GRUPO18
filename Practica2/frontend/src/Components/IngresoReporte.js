a2680749cbcbd136ce907f722e9b21e0184f752e
Nery Eduardo Herrera <tesco.herrera.edu@gmai.com>
2021-10-02 01:28:00 -0600

    [add] dockerfile de frontend

--- Practica2/frontend/src/Components/IngresoReporte.js
+++ Practica2/frontend/src/Components/IngresoReporte.js
@@ -11,7 +11,8 @@ function IngresoReporte() {
     const [reporte, setReporte] = useState('');
 
     const enviar = async() => { 
-        const res = await axios.post(`http://34.125.190.196:8080/api/create/reporte`, {
+        //const res = await axios.post(`http://34.125.190.196:8080/api/create/reporte`, {
+        const res = await axios.post(`http://loadbalancer:8080/api/create/reporte`, {    
             carnet:carnet,
             nombre:nombre, 
             curso:curso, 
